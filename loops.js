988be1c801da3a20a6f6fe7eeb8815ceb63aee4f
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 03:36:02 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array){
+  for(array = 0; array < 25; array++){
+    if(array < 1){
+      return `I am ${array} strange loops`
+    } else{
+      return "I am 1 strange loop"
+    }
+  }
+}
